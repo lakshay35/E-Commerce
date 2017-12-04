26b2a096b83539ca4cba51aa0876c2e52a0c1bbc
DhanashreeJoshi06 <32025690+DhanashreeJoshi06@users.noreply.github.com>
2017-12-04 00:09:14 -0500

    Update ShippingController.java

--- OnlineBookstore/src/logic/ShippingController.java
+++ OnlineBookstore/src/logic/ShippingController.java
@@ -7,11 +7,13 @@ import persistent.OrderDA;
 
 public class ShippingController {
 
+//This function enables shipping employee to view list of orders
 	public List<Order> viewOrders() {
 		// TODO Auto-generated method stub
 		return OrderDA.viewOrders();
 	}
 
+//This functions enables shipping employee to change the order status	
 	public int changeOrderStatus(String orderID, String status) {
 		// TODO Auto-generated method stub
 		Order order = new Order();
